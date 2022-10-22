ecb33fe1cc249427d964b51f2983a1e65e522d48
kpost34 <kpost342005@hotmail.com>
2022-10-21 23:10:53 -0600

    Added to create tables and a scatter plot with a smoother

--- app.R
+++ app.R
@@ -2,30 +2,80 @@
 #Shiny App demo for data that fit an ANCOVA
 
 library(shiny)
+library(tidyverse)
+library(janitor)
+
+data(mtcars)
+
+mtcars<-as_tibble(mtcars) %>%
+  select(-c(cyl,gear,carb)) %>%
+  mutate(across(c(vs,am),~as.factor(.x)))
+
+
 
 # Define UI 
 ui <- fluidPage(
 
-    # Application title
-    titlePanel("ANCOVA Demo"),
+  # Application title
+  titlePanel("ANCOVA Demo"),
 
-    # Sidebar with 
-    sidebarLayout(
-        sidebarPanel(
+  # Sidebar panel layout
+  sidebarLayout(
+    sidebarPanel(width=4,
+      varSelectInput("varSel_num","Select a continuous variable",
+                     data=mtcars %>%
+                       select(where(is.numeric),-mpg)),
+      varSelectInput("varSel_cat","Select a binary variable",
+                     data=mtcars %>%
+                       select(where(is.factor))),
+      checkboxGroupInput("chkGrp_regs","Select one or more regression lines",
+                         choices="All data without groups")
+    ),
+
+    # Show tables and a plot of the data
+    mainPanel(width=8,
+      fluidRow(
+        column(width=4,
+          tableOutput("table1")
         ),
-
-        # Show a plot of the data
-        mainPanel(
-           plotOutput("scatterPlot")
+        column(width=4,
+          tableOutput("table2")
         )
+      ),
+      fluidRow(
+        plotOutput("scatterPlot")
+      )
     )
+  )
 )
 
 # Define server function
 server <- function(input, output, session) {
+  
+  output$table1<-renderTable({
+    mtcars %>%
+      select(!!input$varSel_num,mpg) %>%
+      summarize(across(everything(),~mean(.x)))
+  })
+    
+  output$table2<-renderTable({
+    mtcars %>%
+      select(!!input$varSel_num,!!input$varSel_cat,mpg) %>%
+      group_by(!!input$varSel_cat) %>%
+      summarize(across(everything(),~mean(.x)))
+  })
+    
 
     output$scatterPlot <- renderPlot({
-  
+      ggplot(mtcars,aes(x=!!input$varSel_num,y=mpg,color=!!input$varSel_cat)) +
+        geom_point() +
+        theme_bw() -> p
+      
+      if(!is.null(input$chkGrp_regs)){
+        p + geom_smooth(method="lm")->p
+      }
+      p
+        
     })
 }
 
