9c744bdf5274e66ee5fa4ab2759ac4e91e99ba61
kpost34 <kpost342005@hotmail.com>
2022-10-30 15:21:53 -0600

    Added title to tables, split up 'grouped data' into separate regression models, and displayed ANCOVA output of full model

--- app.R
+++ app.R
@@ -1,8 +1,10 @@
 #Created by Keith Post on 10/18/22
 #Shiny App demo for data that fit an ANCOVA
 
+
 #load packages
-pacman::p_load(shiny,tidyverse,janitor,plotly,broom)
+pacman::p_load(shiny,here,tidyverse,janitor,plotly,broom)
+
 
 #load data
 data(mtcars)
@@ -27,7 +29,7 @@ ui <- fluidPage(
 
   #### Sidebar panel layout
   sidebarLayout(
-    sidebarPanel(width=3,
+    sidebarPanel(width=2,
       selectInput("sel_num","Select a continuous variable",
                   choices=mtcars %>%
                     select(where(is.numeric),-mpg) %>%
@@ -37,30 +39,52 @@ ui <- fluidPage(
                     select(where(is.factor)) %>%
                     names()),
       checkboxGroupInput("chkGrp_regs","Select regression line(s)",
-                         choices=c("No groups"="all","Grouped by binary variable"="by_bi"))
+                         choices=c("No groups"="all","Grouped by binary variable"="by_bi")),
+      checkboxGroupInput("chkGrp_ancova","Run ANCOVA on",
+                         choices=c("Full model with interaction"="full_mod_wIntxn"))
     ),
 
     #### Show tables and a plot of the data
-    mainPanel(width=9,
+    mainPanel(width=10,
+      #summary stats in tables
       fluidRow(
         column(4,
           tableOutput("sum_tab_noGroup")
         ),
-        column(2),
-        column(3,
+        column(1),
+        column(5,
           tableOutput("sum_tab_wGroup")
         )
       ),
+      #add horizontal line
       hr(),
+      #interactive plot
       fluidRow(
         column(5,
           plotlyOutput("scatter_plot")
         ),
-        column(1),
-        column(3,
-          tableOutput("mod_tab_noGroup"),
+        #add vertical space between plot and table(s)
+        # column(1),
+        #model outputs
+        column(5,
+          tableOutput("mod1_tab_noGroup"),
           br(),
-          tableOutput("mod_tab_wGroup")
+          splitLayout(
+            tableOutput("mod2a_tab_wGroup"),
+            tableOutput("mod2b_tab_wGroup")
+            
+          )
+        )
+      ),
+      #add second horizontal line
+      hr(),
+      #anova table
+      fluidRow(
+        column(5,
+          tableOutput("ancova_sum_tab")
+        ),
+        column(5,
+          tableOutput("ancova_anova_tab")
         )
       )
     )
@@ -76,7 +100,9 @@ server <- function(input, output, session) {
     mtcars %>%
       select(!!sym(input$sel_num),mpg) %>%
       summarize(across(everything(),list(mean=mean,sd=sd)))},
-    striped=TRUE,hover=TRUE,caption="Without groups"
+    striped=TRUE,hover=TRUE,
+    caption="Without groups",
+    caption.placement=getOption("xtable.caption.placement","top")
   )
     
   ## Grouped by categorical variable
@@ -85,7 +111,9 @@ server <- function(input, output, session) {
       select(!!sym(input$sel_num),!!sym(input$sel_cat),mpg) %>%
       group_by(!!sym(input$sel_cat)) %>%
       summarize(across(everything(),list(mean=mean,sd=sd)))},
-    striped=TRUE,hover=TRUE,caption="Grouped by binary variable"
+    striped=TRUE,hover=TRUE,
+    caption="Grouped by binary variable",
+    caption.placement=getOption("xtable.caption.placement","top")
   )
 
   ### Output scatterplot
@@ -94,6 +122,7 @@ server <- function(input, output, session) {
     mtcars %>%
       ggplot(aes(label=model)) +
       geom_point(aes(x=!!sym(input$sel_num),y=mpg,color=!!sym(input$sel_cat)),size=2,alpha=0.7) +
+      #expand_limits(x=c(0,NA),y=c(0,NA)) +
       scale_color_viridis_d() +
       theme_bw() +
       theme(text=element_text(size=16)) -> p1
@@ -125,7 +154,7 @@ server <- function(input, output, session) {
   })
   
   
-  ### Create reactive expressions of models
+  ### Reactive expressions of models
   ## Without groups
   mod1<-reactive({
     req(input$chkGrp_regs=="all")
@@ -133,26 +162,75 @@ server <- function(input, output, session) {
       lm(paste0("mpg","~",input$sel_num) %>% as.formula(),data=.)
   })
   
-  ## With groups
-  mod2<-reactive({
+  
+  ## With groups separately
+  # Level 0
+  mod2a<-reactive({
     req(input$chkGrp_regs=="by_bi")
     mtcars %>%
-      lm(paste0("mpg","~",input$sel_num,"+",input$sel_cat) %>% as.formula(),data=.)
+      filter(!!sym(input$sel_cat)==0) %>%
+      lm(paste0("mpg","~",input$sel_num) %>% as.formula(),data=.)
+  })
+  
+  # Level 1
+  mod2b<-reactive({
+    req(input$chkGrp_regs=="by_bi")
+    mtcars %>%
+      filter(!!sym(input$sel_cat)==1) %>%
+      lm(paste0("mpg","~",input$sel_num) %>% as.formula(),data=.)
   })
   
   
-  ### Write output
+  ## With groups with interaction
+  mod3<-reactive({
+    req(input$chkGrp_ancova=="full_mod_wIntxn")
+    mtcars %>%
+      lm(paste0("mpg","~",input$sel_cat,"*",input$sel_num) %>% as.formula(),data=.)
+  })
+  
+  
+  ### Linear models
   ## Without groups
-  output$mod_tab_noGroup<-renderTable({
+  output$mod1_tab_noGroup<-renderTable({
       mod1() %>%
-        tidy()
-    })
+        tidy() %>%
+      select(-c(statistic,p.value))},
+      striped=TRUE,hover=TRUE,
+      caption="Model without groups",
+      caption.placement=getOption("xtable.caption.placement","top")
+  )
   
   ## With groups
-  output$mod_tab_wGroup<-renderTable({
-    mod2() %>%
-      tidy()
-  })
+  # Level 0
+  output$mod2a_tab_wGroup<-renderTable({
+    mod2a() %>%
+      tidy() %>%
+      select(-c(statistic,p.value))},
+    striped=TRUE,hover=TRUE,
+    caption="Binary variable = 0 (purple)",
+    caption.placement=getOption("xtable.caption.placement","top")
+    )
+  
+  # Level 1
+  output$mod2b_tab_wGroup<-renderTable({
+    mod2b() %>%
+      tidy() %>%
+      select(-c(statistic,p.value))},
+    striped=TRUE,hover=TRUE,
+    caption="Binary variable = 1 (yellow)",
+    caption.placement=getOption("xtable.caption.placement","top")
+  )
+  
+  
+  ### ANCOVA output
+  output$ancova_sum_tab<-renderTable({
+    mod3() %>%
+      anova() %>%
+      tidy()},
+    striped=TRUE,hover=TRUE,
+    caption="Full model (with interaction)",
+    caption.placement=getOption("xtable.caption.placement","top")
+  )
   
   
 }
@@ -162,15 +240,25 @@ server <- function(input, output, session) {
 shinyApp(ui = ui, server = server)
 
 
-
-## Done
-## got functionality of smoothers to work
-## improved layout
-## turned scatter plot into a plotly
+## NEXT
+# add 'subtitles' for each section (e.g., Summary Statistics, Linear Models, ANCOVA)
 
 
-## Last commit
-## added code to produce tables, a scatter plot, and a smoother
+## DONE
+# added title tables using renderTable
+# split up 'grouped data' into two separate reg models
+# displayed separate reg models (by group levels) next to each other
+# displayed ANCOVA output full model
+
+
+
+## LAST COMMIT
+# got functionality of smoothers to work
+# improved layout
+# turned scatter plot into a plotly
+# created reactive model objects
+# dynamically display stats of models
+## changed varSelectInputs to selectInputs
 
 
 # mod1<-lm(mpg~disp,data=mtcars)
